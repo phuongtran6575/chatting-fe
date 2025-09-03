50f6a6d24f4b11f4ccd89310180aa337343737f6
tranphuong670 <tranhongphuong272@gmail.com>
2025-09-03 10:53:05 +0700

    fix 4 file

---
+++ chatting/src/assets/page/chat/MessageWindowPage.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const MessageWindowPage = () => {
+  return (
+    <Box>
+        
+    </Box>
+  )
+}
+
+export default MessageWindowPage
