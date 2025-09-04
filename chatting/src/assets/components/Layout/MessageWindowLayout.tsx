45bf49ae540c208efc18b565e3d3d31ce3ca79b9
tranphuong670 <tranhongphuong272@gmail.com>
2025-09-04 16:47:58 +0700

    fix 1 file

--- chatting/src/assets/components/Layout/MessageWindowLayout.tsx
+++ chatting/src/assets/components/Layout/MessageWindowLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import Sidebar from "../Sidebar";
 import ChatHeader from "../Header";
@@ -7,6 +7,36 @@ import MessageComposer from "../MessageComposer";
 
 
 const MessageWindowLayout = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [input, setInput] = useState("");
+  const [ws, setWS] = useState<WebSocket|null>(null)
+  useEffect(()=>{
+    const clienId = Math.floor(Math.random() * 10000);
+    const socket = new WebSocket(`ws://localhost:8000/ws/${clienId}`);
+    socket.onopen = () => {
+      console.log("✅ WebSocket connected");
+    };
+
+    socket.onmessage = (event) => {
+      setMessages((prev) => [...prev, event.data]); // thêm tin nhắn vào state
+    };
+
+    socket.onclose = () => {
+      console.log("❌ WebSocket disconnected");}
+      setWS(socket);
+
+    // cleanup khi unmount
+    return () => {
+      socket.close();
+    };
+    
+  })
+  const sendMessage = () => {
+    if (ws && input.trim() !== "") {
+      ws.send(input); // gửi message lên server
+      setInput("");
+    }
+  };
   return (
     <Box sx={{ display: "flex", bgcolor: "#0f1c33" }}>
       {/* Sidebar */}
